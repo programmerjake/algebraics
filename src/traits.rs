ddb04345df188d0c9b9a30e3ad4d411140e9f840
Jacob Lifshay <programmerjake@gmail.com>
2019-07-20 22:10:45 -0700

    add DivMod trait

--- src/traits.rs
+++ src/traits.rs
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 // See Notices.txt for copyright information
+use num_bigint::{BigInt, BigUint};
 use num_integer::Integer;
+use num_rational::Ratio;
+use num_traits::Zero;
+use std::ops::Div;
+use std::ops::Rem;
 
 pub trait GCD<Rhs = Self> {
     type Output;
@@ -20,4 +25,28 @@ pub trait PolynomialEval<T> {
 
 pub trait Derivative<Output = Self> {
     fn derivative(self) -> Output;
+}
+
+pub trait DivRem<Rhs = Self>: Div<Rhs> + Rem<Rhs> {
+    fn div_rem(&self, rhs: &Rhs) -> (<Self as Div<Rhs>>::Output, <Self as Rem<Rhs>>::Output);
+}
+
+macro_rules! impl_div_rem {
+    ($($t:ty),*) => {
+        $(
+            impl DivRem for $t {
+                fn div_rem(&self, rhs: &Self) -> (Self, Self) {
+                    Integer::div_rem(self, rhs)
+                }
+            }
+        )*
+    };
+}
+
+impl_div_rem!(u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, usize, isize, BigInt, BigUint);
+
+impl<T: Integer + Clone> DivRem for Ratio<T> {
+    fn div_rem(&self, rhs: &Self) -> (Self, Self) {
+        (self / rhs, Zero::zero())
+    }
 }
