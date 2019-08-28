2d1eda11b97e4983ccd952dd43c264561dfdb60a
Jacob Lifshay <programmerjake@gmail.com>
2019-08-28 01:46:32 -0700

    distinct_degree_factorization works

--- src/polynomial/distinct_degree_factorization.rs
+++ src/polynomial/distinct_degree_factorization.rs
@@ -22,10 +22,90 @@ where
             None => return Vec::new(),
             Some(v) => v,
         };
-        let modulus = nonzero_highest_power_coefficient.modulus().into_modulus();
+        let one_coefficient = ModularInteger::new(
+            V::one(),
+            nonzero_highest_power_coefficient.modulus().clone(),
+        );
+        let x = Polynomial::make_monomial(one_coefficient, 1);
+        let characteristic = nonzero_highest_power_coefficient.modulus().into_modulus();
         self /= &nonzero_highest_power_coefficient; // convert to monic
         let mut retval = vec![nonzero_highest_power_coefficient.into()];
-        unimplemented!();
-        retval
+        let mut power = x.powmod(characteristic.clone(), &self);
+        for _ in 0..self.len() {
+            if self.len() <= 1 {
+                return retval;
+            }
+            let next_factor = (&power - &x).gcd(&self);
+            self /= &next_factor;
+            retval.push(next_factor);
+            power = power.powmod(characteristic.clone(), &self);
+        }
+        unreachable!("failed to find all factors")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::mod_int::KnownPrime;
+
+    #[test]
+    fn test_distinct_degree_factorization() {
+        fn make_poly(
+            poly: &[i32],
+            modulus: KnownPrime<i32>,
+        ) -> Polynomial<ModularInteger<i32, KnownPrime<i32>>> {
+            poly.iter()
+                .map(|&coefficient| ModularInteger::new(coefficient, modulus))
+                .collect()
+        }
+        fn test_case(poly: &[i32], expected_factors: &[&[i32]], modulus: KnownPrime<i32>) {
+            let poly = make_poly(poly, modulus);
+            let expected_factors: Vec<_> = expected_factors
+                .iter()
+                .map(|poly| make_poly(*poly, modulus))
+                .collect();
+            println!("poly: {}", poly);
+            println!("expected_factors:");
+            for factor in &expected_factors {
+                println!("    {}", factor);
+            }
+            let factors = poly.distinct_degree_factorization();
+            println!("factors:");
+            for factor in &factors {
+                println!("    {}", factor);
+            }
+            assert!(expected_factors == factors);
+        }
+        test_case(
+            &[5, 0, 6, 6, 0, 0, 6, 0, 4, 0, 5, 6, 3, 3],
+            &[
+                &[3],
+                &[6, 4, 6, 1],
+                &[6, 3, 2, 4, 1],
+                &[4, 0, 0, 2, 6, 5, 1],
+            ],
+            KnownPrime::new_unsafe(7),
+        );
+        test_case(
+            &[4, 2, 0, 0, 2, 2, 4, 6, 0, 5, 1, 3, 6, 3],
+            &[
+                &[3],
+                &[1],
+                &[1],
+                &[1],
+                &[1],
+                &[1],
+                &[1],
+                &[1],
+                &[1],
+                &[1],
+                &[1],
+                &[1],
+                &[1],
+                &[6, 3, 0, 0, 3, 3, 6, 2, 0, 4, 5, 1, 2, 1],
+            ],
+            KnownPrime::new_unsafe(7),
+        );
     }
 }
