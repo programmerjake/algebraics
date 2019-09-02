05e2e2bf97940328776bb1a64c4e99212585d10f
Jacob Lifshay <programmerjake@gmail.com>
2019-09-02 04:13:03 -0700

    add constructing factor tree to prepare for hensel lifting

--- src/polynomial/factorization_over_integers.rs
+++ src/polynomial/factorization_over_integers.rs
@@ -5,23 +5,116 @@ use crate::mod_int::KnownOddPrime;
 use crate::mod_int::ModularInteger;
 use crate::polynomial::Polynomial;
 use crate::polynomial::PolynomialCoefficient;
-use crate::polynomial::PolynomialDivSupported;
 use crate::polynomial::PolynomialFactor;
 use crate::polynomial::PolynomialFactors;
-use crate::polynomial::PolynomialReducingFactorSupported;
-use crate::polynomial::SquareFreePolynomialFactors;
-use crate::traits::CharacteristicZero;
 use crate::traits::ExactDiv;
-use crate::traits::RingCharacteristic;
-use crate::traits::GCD;
 use crate::util::next_prime_i32;
+use crate::util::LeafOrNodePair;
+use crate::util::PrintTree;
+use crate::util::PrintTreeData;
 use num_bigint::BigInt;
-use num_bigint::ToBigInt;
 use num_integer::Integer;
 use num_rational::Ratio;
 use rand::Rng;
 use rand::SeedableRng;
 use rand_pcg::Pcg64Mcg;
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+use std::fmt;
+use std::mem;
+
+struct FactorTreeInteriorNode<T: PolynomialCoefficient> {
+    left: FactorTreeNode<T>,
+    right: FactorTreeNode<T>,
+    total_degree: usize,
+}
+
+impl<T: PolynomialCoefficient> FactorTreeInteriorNode<T> {
+    fn new(left: FactorTreeNode<T>, right: FactorTreeNode<T>) -> Self {
+        let total_degree = left.total_degree() + right.total_degree();
+        Self {
+            left,
+            right,
+            total_degree,
+        }
+    }
+}
+
+struct FactorTreeLeafNode<T: PolynomialCoefficient> {
+    factor: Polynomial<T>,
+}
+
+impl<T: PolynomialCoefficient> FactorTreeLeafNode<T> {
+    fn total_degree(&self) -> usize {
+        self.factor.degree().expect("non-zero factor")
+    }
+}
+
+enum FactorTreeNode<T: PolynomialCoefficient> {
+    Interior(Box<FactorTreeInteriorNode<T>>),
+    Leaf(FactorTreeLeafNode<T>),
+}
+
+impl<T: PolynomialCoefficient> FactorTreeNode<T> {
+    fn total_degree(&self) -> usize {
+        match self {
+            Self::Interior(node) => node.total_degree,
+            Self::Leaf(node) => node.total_degree(),
+        }
+    }
+}
+
+impl<T: PolynomialCoefficient + fmt::Display> fmt::Display for FactorTreeLeafNode<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({})", self.factor)
+    }
+}
+
+impl<T: PolynomialCoefficient + fmt::Display> fmt::Display for FactorTreeInteriorNode<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "FactorTreeInteriorNode{{left:.., right:.., total_degree:{}}}",
+            self.total_degree
+        )
+    }
+}
+
+impl<'a, T: 'a + PolynomialCoefficient + fmt::Display> PrintTreeData<'a> for FactorTreeNode<T> {
+    type Leaf = &'a FactorTreeLeafNode<T>;
+    type NodeData = &'a FactorTreeInteriorNode<T>;
+}
+
+impl<T: 'static + PolynomialCoefficient + fmt::Display> PrintTree for FactorTreeNode<T> {
+    fn to_leaf_or_node_pair(
+        &self,
+    ) -> LeafOrNodePair<&FactorTreeLeafNode<T>, &Self, &FactorTreeInteriorNode<T>> {
+        match self {
+            Self::Interior(node) => LeafOrNodePair::NodePair(&node.left, &**node, &node.right),
+            Self::Leaf(node) => LeafOrNodePair::Leaf(node),
+        }
+    }
+}
+
+impl<T: PolynomialCoefficient> Eq for FactorTreeNode<T> {}
+
+impl<T: PolynomialCoefficient> PartialEq for FactorTreeNode<T> {
+    fn eq(&self, rhs: &Self) -> bool {
+        self.cmp(rhs) == Ordering::Equal
+    }
+}
+
+impl<T: PolynomialCoefficient> PartialOrd for FactorTreeNode<T> {
+    fn partial_cmp(&self, rhs: &Self) -> Option<Ordering> {
+        Some(self.cmp(rhs))
+    }
+}
+
+impl<T: PolynomialCoefficient> Ord for FactorTreeNode<T> {
+    fn cmp(&self, rhs: &Self) -> Ordering {
+        self.total_degree().cmp(&rhs.total_degree()).reverse()
+    }
+}
 
 impl Polynomial<BigInt> {
     pub(crate) fn factor_square_free_polynomial_with_rng<R: Rng + ?Sized>(
@@ -70,11 +163,39 @@ impl Polynomial<BigInt> {
                     poly.same_degree_factorization(factor_degree, rng)
                 }
             })
+            .map(|factor| FactorTreeNode::Leaf(FactorTreeLeafNode { factor }))
             .collect();
+
         println!("modular_factors:");
         for factor in &modular_factors {
-            println!("    {}", factor);
+            if let FactorTreeNode::Leaf(leaf) = factor {
+                println!("    {}", leaf.factor);
+            } else {
+                unreachable!("known to be all leaf nodes");
+            }
         }
+
+        // construct factor tree
+        let mut factor_tree_heap: BinaryHeap<_> = modular_factors.into();
+
+        let factor_tree = loop {
+            let smallest_factor_tree = match factor_tree_heap.pop() {
+                None => return vec![],
+                Some(v) => v,
+            };
+            let second_smallest_factor_tree = match factor_tree_heap.pop() {
+                None => break smallest_factor_tree,
+                Some(v) => v,
+            };
+            factor_tree_heap.push(FactorTreeNode::Interior(
+                FactorTreeInteriorNode::new(smallest_factor_tree, second_smallest_factor_tree)
+                    .into(),
+            ))
+        };
+        mem::drop(factor_tree_heap);
+
+        factor_tree.print_tree();
+
         // factor using last portion of Berlekamp-Zassenhaus algorithm
         unimplemented!()
     }
