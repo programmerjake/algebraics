eb6518db9806b5437f5942387ef23751cfe4310c
Jacob Lifshay <programmerjake@gmail.com>
2019-09-03 20:05:48 -0700

    working on factorization_over_integers.rs

--- src/polynomial/factorization_over_integers.rs
+++ src/polynomial/factorization_over_integers.rs
@@ -3,18 +3,26 @@
 
 use crate::mod_int::KnownOddPrime;
 use crate::mod_int::ModularInteger;
+use crate::mod_int::Modulus;
 use crate::polynomial::Polynomial;
 use crate::polynomial::PolynomialCoefficient;
+use crate::polynomial::PolynomialDivSupported;
 use crate::polynomial::PolynomialFactor;
 use crate::polynomial::PolynomialFactors;
+use crate::polynomial::PolynomialReducingFactorSupported;
 use crate::traits::ExactDiv;
+use crate::traits::ExtendedGCD;
+use crate::traits::ExtendedGCDResult;
 use crate::util::next_prime_i32;
 use crate::util::LeafOrNodePair;
 use crate::util::PrintTree;
 use crate::util::PrintTreeData;
 use num_bigint::BigInt;
 use num_integer::Integer;
+use num_integer::Roots;
 use num_rational::Ratio;
+use num_traits::One;
+use num_traits::Signed;
 use rand::Rng;
 use rand::SeedableRng;
 use rand_pcg::Pcg64Mcg;
@@ -27,16 +35,53 @@ struct FactorTreeInteriorNode<T: PolynomialCoefficient> {
     left: FactorTreeNode<T>,
     right: FactorTreeNode<T>,
     total_degree: usize,
+    product: Polynomial<T>,
+    left_bezout_coefficient: Polynomial<T>,
+    right_bezout_coefficient: Polynomial<T>,
 }
 
 impl<T: PolynomialCoefficient> FactorTreeInteriorNode<T> {
-    fn new(left: FactorTreeNode<T>, right: FactorTreeNode<T>) -> Self {
+    fn new(left: FactorTreeNode<T>, right: FactorTreeNode<T>) -> Self
+    where
+        T: PolynomialDivSupported + PolynomialReducingFactorSupported,
+    {
         let total_degree = left.total_degree() + right.total_degree();
+        let left_product = left.product();
+        let right_product = right.product();
+        let product = left_product * right_product;
+        let ExtendedGCDResult {
+            gcd,
+            x: left_bezout_coefficient,
+            y: right_bezout_coefficient,
+        } = left_product.extended_gcd(right_product);
+        assert!(gcd.is_one());
+        assert!(left_bezout_coefficient.len() < right_product.len());
+        assert!(right_bezout_coefficient.len() < left_product.len());
         Self {
             left,
             right,
             total_degree,
+            product,
+            left_bezout_coefficient,
+            right_bezout_coefficient,
         }
+    }
+    fn map_coefficients<O: PolynomialCoefficient, F: FnMut(T) -> O>(
+        &self,
+        f: &mut F,
+    ) -> FactorTreeInteriorNode<O> {
+        FactorTreeInteriorNode {
+            left: self.left.map_coefficients(f),
+            right: self.right.map_coefficients(f),
+            total_degree: self.total_degree,
+            product: self.product.iter().map(&mut *f).collect(),
+            left_bezout_coefficient: self.left_bezout_coefficient.iter().map(&mut *f).collect(),
+            right_bezout_coefficient: self.right_bezout_coefficient.iter().map(&mut *f).collect(),
+        }
+    }
+    fn into_leaves(self, leaves: &mut Vec<Polynomial<T>>) {
+        self.left.into_leaves(leaves);
+        self.right.into_leaves(leaves);
     }
 }
 
@@ -47,6 +92,17 @@ struct FactorTreeLeafNode<T: PolynomialCoefficient> {
 impl<T: PolynomialCoefficient> FactorTreeLeafNode<T> {
     fn total_degree(&self) -> usize {
         self.factor.degree().expect("non-zero factor")
+    }
+    fn map_coefficients<O: PolynomialCoefficient, F: FnMut(T) -> O>(
+        &self,
+        f: &mut F,
+    ) -> FactorTreeLeafNode<O> {
+        FactorTreeLeafNode {
+            factor: self.factor.iter().map(f).collect(),
+        }
+    }
+    fn into_leaves(self, leaves: &mut Vec<Polynomial<T>>) {
+        leaves.push(self.factor);
     }
 }
 
@@ -60,6 +116,29 @@ impl<T: PolynomialCoefficient> FactorTreeNode<T> {
         match self {
             Self::Interior(node) => node.total_degree,
             Self::Leaf(node) => node.total_degree(),
+        }
+    }
+    fn product(&self) -> &Polynomial<T> {
+        match self {
+            Self::Interior(node) => &node.product,
+            Self::Leaf(node) => &node.factor,
+        }
+    }
+    fn map_coefficients<O: PolynomialCoefficient, F: FnMut(T) -> O>(
+        &self,
+        f: &mut F,
+    ) -> FactorTreeNode<O> {
+        match self {
+            FactorTreeNode::Interior(node) => {
+                FactorTreeNode::Interior(node.map_coefficients(f).into())
+            }
+            FactorTreeNode::Leaf(node) => FactorTreeNode::Leaf(node.map_coefficients(f)),
+        }
+    }
+    fn into_leaves(self, leaves: &mut Vec<Polynomial<T>>) {
+        match self {
+            Self::Interior(node) => node.into_leaves(leaves),
+            Self::Leaf(node) => node.into_leaves(leaves),
         }
     }
 }
@@ -113,6 +192,75 @@ impl<T: PolynomialCoefficient> PartialOrd for FactorTreeNode<T> {
 impl<T: PolynomialCoefficient> Ord for FactorTreeNode<T> {
     fn cmp(&self, rhs: &Self) -> Ordering {
         self.total_degree().cmp(&rhs.total_degree()).reverse()
+    }
+}
+
+impl FactorTreeLeafNode<ModularInteger<BigInt, BigInt>> {
+    fn hensel_lift_step(
+        &mut self,
+        _new_modulus: &BigInt,
+        new_product: Polynomial<ModularInteger<BigInt, BigInt>>,
+    ) {
+        self.factor = new_product;
+    }
+}
+
+impl FactorTreeInteriorNode<ModularInteger<BigInt, BigInt>> {
+    fn hensel_lift_step(
+        &mut self,
+        new_modulus: &BigInt,
+        new_product: Polynomial<ModularInteger<BigInt, BigInt>>,
+    ) {
+        fn set_modulus<T: IntoIterator<Item = ModularInteger<BigInt, BigInt>>>(
+            value: T,
+            new_modulus: &BigInt,
+        ) -> Polynomial<ModularInteger<BigInt, BigInt>> {
+            value
+                .into_iter()
+                .map(Into::into)
+                .map(|(value, _modulus)| ModularInteger::new(value, new_modulus.clone()))
+                .collect()
+        }
+        let f = new_product;
+        let g = set_modulus(self.left.product(), new_modulus);
+        let h = set_modulus(self.right.product(), new_modulus);
+        let s = set_modulus(
+            mem::replace(&mut self.left_bezout_coefficient, Default::default()),
+            new_modulus,
+        );
+        let t = set_modulus(
+            mem::replace(&mut self.right_bezout_coefficient, Default::default()),
+            new_modulus,
+        );
+        let e = &f - &g * &h;
+        let (q, r) = (&s * &e).div_rem(&h);
+        let g_star = &g + &t * e + q * g;
+        let h_star = h + r;
+        let b =
+            &s * &g_star + &t * &h_star - ModularInteger::new(BigInt::one(), new_modulus.clone());
+        let (c, d) = (&s * &b).div_rem(&h_star);
+        let s_star = s - d;
+        let t_mul_b = &t * b;
+        let t_star = t - t_mul_b - c * &g_star;
+        self.product = f;
+        self.left_bezout_coefficient = s_star;
+        self.right_bezout_coefficient = t_star;
+        self.left.hensel_lift_step(new_modulus, g_star);
+        self.right.hensel_lift_step(new_modulus, h_star);
+        self.total_degree = self.left.total_degree() + self.right.total_degree();
+    }
+}
+
+impl FactorTreeNode<ModularInteger<BigInt, BigInt>> {
+    fn hensel_lift_step(
+        &mut self,
+        new_modulus: &BigInt,
+        new_product: Polynomial<ModularInteger<BigInt, BigInt>>,
+    ) {
+        match self {
+            Self::Leaf(node) => node.hensel_lift_step(new_modulus, new_product),
+            Self::Interior(node) => node.hensel_lift_step(new_modulus, new_product),
+        }
     }
 }
 
@@ -175,6 +323,8 @@ impl Polynomial<BigInt> {
             }
         }
 
+        let modular_factors_len = modular_factors.len();
+
         // construct factor tree
         let mut factor_tree_heap: BinaryHeap<_> = modular_factors.into();
 
@@ -193,8 +343,52 @@ impl Polynomial<BigInt> {
             ))
         };
         mem::drop(factor_tree_heap);
+        let mut modulus = BigInt::from(modulus.into_modulus());
+        let mut factor_tree = factor_tree.map_coefficients(&mut |coefficient: ModularInteger<
+            i32,
+            KnownOddPrime<i32>,
+        >| {
+            ModularInteger::<BigInt, BigInt>::new(
+                BigInt::from(*coefficient.value()),
+                modulus.clone(),
+            )
+        });
 
         factor_tree.print_tree();
+        println!();
+
+        let inf_norm = self
+            .iter()
+            .map(|v| v.abs())
+            .max()
+            .expect("known to not be empty");
+        let mut ceil_sqrt_degree_plus_one = (degree + 1).sqrt();
+        if ceil_sqrt_degree_plus_one * ceil_sqrt_degree_plus_one < degree + 1 {
+            ceil_sqrt_degree_plus_one += 1;
+        }
+        let factor_coefficient_limit = (BigInt::from(ceil_sqrt_degree_plus_one) << degree)
+            * inf_norm
+            * self.highest_power_coefficient();
+        let needed_modulus = factor_coefficient_limit * 2i32 + 1i32;
+        while modulus < needed_modulus {
+            modulus = &modulus * &modulus;
+            let expected_product: Polynomial<_> = self
+                .iter()
+                .map(|coefficient| ModularInteger::new(coefficient, modulus.clone()))
+                .collect();
+            factor_tree.hensel_lift_step(&modulus, expected_product);
+
+            factor_tree.print_tree();
+            println!();
+        }
+
+        let mut modular_factors = Vec::with_capacity(modular_factors_len);
+        factor_tree.into_leaves(&mut modular_factors);
+
+        println!("modular_factors:");
+        for factor in &modular_factors {
+            println!("    {}", factor);
+        }
 
         // factor using last portion of Berlekamp-Zassenhaus algorithm
         unimplemented!()
